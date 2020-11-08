6821bb18b0b6bad8817cfdcaae277ca15ade0a37
Youdiowei Eteimorde <45427673+EteimZ@users.noreply.github.com>
2020-11-08 22:06:21 +0100

    Update model.py

--- model.py
+++ model.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 import string
 import joblib
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
+import nltk
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 # Download StopWords
-nltk.download('stopwords')
+stopwords = nltk.download('stopwords')
 
 #Loading the data
 df = pd.read_csv('spam.csv',
